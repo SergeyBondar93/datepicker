7980d6393c888b027598a5fb71eba1b43d09acf1
sergeybondar93 <sergeybondar93@github.com>
2019-12-20 00:48:51 +0300

    Updates

---
+++ precache-manifest.3c434b97e52d78aeaf4492d8841ccac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecc12f0f0967ab320207dd06b242523",
+    "url": "/datepicker/index.html"
+  },
+  {
+    "revision": "563e9c2d37185040deb3",
+    "url": "/datepicker/static/css/main.4e340104.chunk.css"
+  },
+  {
+    "revision": "30609d1164094fd47993",
+    "url": "/datepicker/static/js/2.6d5c8f87.chunk.js"
+  },
+  {
+    "revision": "563e9c2d37185040deb3",
+    "url": "/datepicker/static/js/main.428e757f.chunk.js"
+  },
+  {
+    "revision": "cf0117609480abd23dc5",
+    "url": "/datepicker/static/js/runtime-main.68cdb28f.js"
+  }
+]);
